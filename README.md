ba7ec8eeef5a4d9a0ba580ab2e10ec69ee43ce00
Igor Savin <iselwin@gmail.com>
2016-08-25 19:01:05 +0300

    Fix "this" reference error, add example to documentation.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # knex-timemachine
 
 Simple library for automatically rollbacking knex transactions at the end of an automated test.
-See "src\timemachine.js" for usage explanation.
+
+Example usage:
+
+let knexTimemachine = new (require('knex-timemachine'));
+
+<...>
+
+var oldKnex = daoObject.getKnex();
+  beforeEach(function (done) {
+    knexTimemachine.startTransaction(done, oldKnex, [daoObject]);
+  });
+
+  afterEach(function (done) {
+    knexTimemachine.rollbackTransaction(done, oldKnex);
+  });
--- src/timemachine.js
+++ src/timemachine.js
@@ -7,9 +7,6 @@ function Timemachine() {
   this.knexUsers;
 }
 
-Timemachine.prototype.startTransaction = startTransaction;
-Timemachine.prototype.rollbackTransaction = rollbackTransaction;
-
 /**
  * Initiates transaction for later rollback. Use it in beforeEach statement as the last statement since it will
  * invoke "done" after completion.
@@ -17,17 +14,18 @@ Timemachine.prototype.rollbackTransaction = rollbackTransaction;
  * @param knex - knex instance that should be used for producing the transaction
  * @param knexUsers - should implement "setKnex" method that accepts Knex instance as a parameter
  */
-function startTransaction(done, knex, knexUsers) {
+Timemachine.prototype.startTransaction = function (done, knex, knexUsers) {
+  var self = this;
   knex.transaction(function (newTrx) {
-    this.trx = newTrx;
-    this.knexUsers = knexUsers;
+    self.trx = newTrx;
+    self.knexUsers = knexUsers;
     for (x in knexUsers) {
       knexUser = knexUsers[x];
       if (typeof knexUser.setKnex !== 'function') {
         throw ("knexUser " + x + " does not implement setKnex function.");
       }
 
-      knexUser.setKnex(trx);
+      knexUser.setKnex(self.trx);
     }
     done();
   })
@@ -42,8 +40,8 @@ function startTransaction(done, knex, knexUsers) {
  * @param done afterEach callback
  * @param knex original knex that should be set back for all users
  */
-function rollbackTransaction(done, knex) {
-  trx.rollback().then(function () {
+Timemachine.prototype.rollbackTransaction = function (done, knex) {
+  this.trx.rollback().then(function () {
     for (x in this.knexUsers) {
       knexUsers[x].setKnex(knex);
     }
